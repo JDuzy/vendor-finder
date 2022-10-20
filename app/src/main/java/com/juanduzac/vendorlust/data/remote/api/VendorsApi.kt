aee2784477658755c7e507f3a0657f11b3feeb06
Juan Duzac <juan.duzac@naranjax.com>
2022-10-19 23:52:48 -0300

    Added Dagger-Hilt modules for dependency injections
    - Set up DI for Retrofit client on AppModule.kt
    - Set up DI for VendorRepository on RepositoryModule.kt
    - Added base url to RetrofitClient
    - Added INTERNET PERMISSION for retrofit

--- app/src/main/java/com/juanduzac/vendorlust/data/remote/api/VendorsApi.kt
+++ app/src/main/java/com/juanduzac/vendorlust/data/remote/api/VendorsApi.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 
 interface VendorsApi {
 
-    @GET("https://gist.githubusercontent.com/F-3r/e7c9c0f5fb3e23b45573c6123499e563/raw/50d7dd2170ad1bd79b964d94567f17187449ea4c/vendorlust.json")
+    @GET("F-3r/e7c9c0f5fb3e23b45573c6123499e563/raw/50d7dd2170ad1bd79b964d94567f17187449ea4c/vendorlust.json")
     suspend fun getVendors() : VendorsResponseDto
 
 }
