54cf30957e4598d0634b70d62b88b69ce3e08d2b
Juan Duzac <juan.duzac@naranjax.com>
2022-10-21 15:11:51 -0300

    Created getVendorsList() and searchVendor() methos on VendorListViewModel

--- app/src/main/java/com/juanduzac/vendorlust/presentation/usecases/vendordetail/VendorListViewModel.kt
+++ app/src/main/java/com/juanduzac/vendorlust/presentation/usecases/vendordetail/VendorListViewModel.kt
@@ -2,28 +2,55 @@ package com.juanduzac.vendorlust.presentation.usecases.vendordetail
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.juanduzac.vendorlust.domain.model.VendorsResponse
 import com.juanduzac.vendorlust.domain.repository.VendorsRepository
+import com.juanduzac.vendorlust.domain.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class VendorListViewModel @Inject constructor(
     private val repository: VendorsRepository
 ) : ViewModel() {
 
-    val vendorsResponse by mutableStateOf(VendorsResponse())
-    val isLoading by mutableStateOf(false)
-    val isRefreshing by mutableStateOf(false)
-    val searchQuery by mutableStateOf("")
+    var vendorsResponse by mutableStateOf(VendorsResponse())
+    var isLoading by mutableStateOf(false)
+    var isRefreshing by mutableStateOf(false)
+    var searchQuery by mutableStateOf("")
 
-    fun getVendorList(query: String = searchQuery, fetchFromRemote: Boolean = false) {
+    private var searchJob: Job? = null
+
+    fun getVendorsList(fetchFromRemote: Boolean = false, query: String = searchQuery) {
         viewModelScope.launch {
-            repository.getVendors()
+            repository
+                .getVendors(fetchFromRemote, query)
+                .collect { result ->
+                    when (result) {
+                        is Resource.Success -> {
+                            result.data?.let { response ->
+                                vendorsResponse = response
+                            }
+                        }
+                        is Resource.Error -> Unit // TODO Feedback
+                        is Resource.Loading -> {
+                            isLoading = true
+                        }
+                    }
+                }
         }
     }
 
+    fun searchVendor() {
+        searchJob?.cancel()
+        searchJob = viewModelScope.launch {
+            delay(500L)
+            getVendorsList()
+        }
+    }
 }
