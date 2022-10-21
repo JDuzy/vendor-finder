d63165a7a0751621f09c31716e9a3f092da805ad
Juan Duzac <juan.duzac@naranjax.com>
2022-10-21 10:09:39 -0300

    Changed VendorRepository interface contract in order to have a query and fetchFromRemote parameters when fetchingVendors

--- app/src/main/java/com/juanduzac/vendorlust/domain/repository/VendorsRepository.kt
+++ app/src/main/java/com/juanduzac/vendorlust/domain/repository/VendorsRepository.kt
@@ -2,8 +2,12 @@ package com.juanduzac.vendorlust.domain.repository
 
 import com.juanduzac.vendorlust.domain.model.VendorsResponse
 import com.juanduzac.vendorlust.domain.util.Resource
+import kotlinx.coroutines.flow.Flow
 
 interface VendorsRepository {
 
-    suspend fun getVendors(): Resource<VendorsResponse>
+    suspend fun getVendors(
+        fetchFromRemote: Boolean,
+        query: String
+    ): Flow<Resource<VendorsResponse>>
 }
