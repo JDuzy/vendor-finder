30c2a1f1ae3db5cfba700226c6f1cf88f694d66d
Juan Duzac <juan.duzac@naranjax.com>
2022-10-21 12:28:54 -0300

    Added insertion of the rest of nested entities when inserting a Vendor

--- app/src/main/java/com/juanduzac/vendorlust/data/repository/VendorsRepositoryImpl.kt
+++ app/src/main/java/com/juanduzac/vendorlust/data/repository/VendorsRepositoryImpl.kt
@@ -1,9 +1,15 @@
 package com.juanduzac.vendorlust.data.repository
 
 import com.juanduzac.vendorlust.data.local.VendorDatabase
+import com.juanduzac.vendorlust.data.mappers.toContactInfoEntity
+import com.juanduzac.vendorlust.data.mappers.toGalleryItemEntity
+import com.juanduzac.vendorlust.data.mappers.toImageEntity
+import com.juanduzac.vendorlust.data.mappers.toOpeningHoursInDayEntity
 import com.juanduzac.vendorlust.data.mappers.toVendor
 import com.juanduzac.vendorlust.data.mappers.toVendorEntity
 import com.juanduzac.vendorlust.data.remote.api.VendorsApi
+import com.juanduzac.vendorlust.data.remote.dtos.OpeningHoursInWeekDto
+import com.juanduzac.vendorlust.data.remote.dtos.VendorDto
 import com.juanduzac.vendorlust.domain.model.VendorsResponse
 import com.juanduzac.vendorlust.domain.repository.VendorsRepository
 import com.juanduzac.vendorlust.domain.util.Resource
@@ -11,6 +17,7 @@ import java.io.IOException
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 
@@ -20,16 +27,16 @@ class VendorsRepositoryImpl @Inject constructor(
     private val db: VendorDatabase
 ) : VendorsRepository {
 
-    private val vendorsDao = db.vendorDao
+    private val vendorDao = db.vendorDao
 
     override suspend fun getVendors(
         fetchFromRemote: Boolean,
         query: String
     ): Flow<Resource<VendorsResponse>> {
         return flow {
-            emit(Resource.Loading(true))
+            emit(Resource.Loading<VendorsResponse>(true))
 
-            val localVendorsEntities = vendorsDao.searchVendorsWithOpeningHoursAndHeroImage(query)
+            val localVendorsEntities = vendorDao.searchVendorsWithOpeningHoursAndHeroImage(query)
             val vendorsModel = localVendorsEntities.map { it.toVendor() }
 
             emit(
@@ -42,35 +49,129 @@ class VendorsRepositoryImpl @Inject constructor(
             val shouldLoadFromCache = !isDbEmpty && !fetchFromRemote
 
             if (shouldLoadFromCache) {
-                emit(Resource.Loading(false))
+                emit(Resource.Loading<VendorsResponse>(false))
                 return@flow
             }
 
-            val remoteVendorResponseDto = try {
-                api.getVendors()
-            } catch (e: IOException) {
-                e.printStackTrace()
-                emit(Resource.Error("IO error, couldn't load data"))
-                null
-            } catch (e: HttpException) {
-                e.printStackTrace()
-                emit(Resource.Error("Http error, couldn't load data"))
-                null
-            }
+            val remoteVendorsDto = fetchVendorsFromRemote(this)
 
-            remoteVendorResponseDto?.let { vendorResponseDto ->
-                vendorsDao.clearVendors()
-                val vendorsEntities = vendorResponseDto.vendors?.map { it.toVendorEntity() }
-                vendorsEntities?.let { entities ->
-                    vendorsDao.insertVendors(entities)
+            remoteVendorsDto?.let { vendorsDto ->
+                vendorDao.clearVendors()
+                insertVendorsInCascade(vendorsDto)
+                val vendorsEntities = vendorsDto.map { it.toVendorEntity() }
+                vendorsEntities.let { entities ->
+                    vendorDao.insertVendors(entities)
                 }
                 // Single source of truth, always coming from cache
-                val localVendors = vendorsDao.searchVendorsWithOpeningHoursAndHeroImage("")
+                val localVendors = vendorDao.searchVendorsWithOpeningHoursAndHeroImage("")
                 val response = VendorsResponse(localVendors.map { it.toVendor() })
 
-                emit(Resource.Success(response))
+                emit(Resource.Success<VendorsResponse>(response))
                 emit(Resource.Loading<VendorsResponse>(false))
+            }
+        } as Flow<Resource<VendorsResponse>>
+    }
+
+    private suspend fun insertVendorsInCascade(vendorDtos: List<VendorDto>) {
+        vendorDtos.forEach { vendorDto ->
+            with(db) {
+
+                vendorDto.contactInfoDto?.let { contactInfoDto ->
+                    contactInfoDao.insertContactInfo(contactInfoDto.toContactInfoEntity(vendorDto.id))
+                }
+
+                vendorDto.galleryDto?.let { galleryItemDtos ->
+                    galleryItemDtos.forEach { galleryItemDto ->
+
+                        galleryItemDto.imageDto?.let { imageDto ->
+                            imageDao.insertImage(
+                                imageDto.toImageEntity(
+                                    galleryItemId = galleryItemDto.id
+                                )
+                            )
+                        }
+
+                        galleryItemDao.insertGalleryItem(
+                            galleryItemDto.toGalleryItemEntity(
+                                vendorDto.id
+                            )
+                        )
+                    }
+                }
+
+                vendorDto.heroImageDto?.let { imageDto ->
+                    imageDao.insertImage(imageDto.toImageEntity(vendorId = vendorDto.id))
+                }
+
+                vendorDto.openingHoursDto?.let { openingHoursInWeekDto ->
+                    insertOpeningHours(openingHoursInWeekDto)
+                }
+
+                vendorDao.insertVendor(vendorDto.toVendorEntity())
+
             }
         }
     }
+
+    private suspend fun insertOpeningHours(openingHoursInWeekDto: OpeningHoursInWeekDto) {
+        openingHoursInWeekDto.monday?.forEach { openingHoursInDayDto ->
+            db.openingHoursInDayDao.insertOpeningHoursInDay(
+                openingHoursInDayDto.toOpeningHoursInDayEntity("monday", openingHoursInWeekDto.id)
+            )
+        }
+
+        openingHoursInWeekDto.thursday?.forEach { openingHoursInDayDto ->
+            db.openingHoursInDayDao.insertOpeningHoursInDay(
+                openingHoursInDayDto.toOpeningHoursInDayEntity("thursday", openingHoursInWeekDto.id)
+            )
+        }
+
+        openingHoursInWeekDto.wednesday?.forEach { openingHoursInDayDto ->
+            db.openingHoursInDayDao.insertOpeningHoursInDay(
+                openingHoursInDayDto.toOpeningHoursInDayEntity(
+                    "wednesday",
+                    openingHoursInWeekDto.id
+                )
+            )
+        }
+
+        openingHoursInWeekDto.tuesday?.forEach { openingHoursInDayDto ->
+            db.openingHoursInDayDao.insertOpeningHoursInDay(
+                openingHoursInDayDto.toOpeningHoursInDayEntity("tuesday", openingHoursInWeekDto.id)
+            )
+        }
+
+        openingHoursInWeekDto.friday?.forEach { openingHoursInDayDto ->
+            db.openingHoursInDayDao.insertOpeningHoursInDay(
+                openingHoursInDayDto.toOpeningHoursInDayEntity("friday", openingHoursInWeekDto.id)
+            )
+        }
+
+        openingHoursInWeekDto.saturday?.forEach { openingHoursInDayDto ->
+            db.openingHoursInDayDao.insertOpeningHoursInDay(
+                openingHoursInDayDto.toOpeningHoursInDayEntity("saturday", openingHoursInWeekDto.id)
+            )
+        }
+
+        openingHoursInWeekDto.sunday?.forEach { openingHoursInDayDto ->
+            db.openingHoursInDayDao.insertOpeningHoursInDay(
+                openingHoursInDayDto.toOpeningHoursInDayEntity("sunday", openingHoursInWeekDto.id)
+            )
+        }
+    }
+
+    private suspend fun fetchVendorsFromRemote(flowCollector: FlowCollector<Any>): List<VendorDto>? {
+        return try {
+            api.getVendors()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            flowCollector.emit(Resource.Error<VendorsResponse>("IO error, couldn't load data"))
+            null
+        } catch (e: HttpException) {
+            e.printStackTrace()
+            flowCollector.emit(Resource.Error<VendorsResponse>("Http error, couldn't load data"))
+            null
+        }
+    }
+
 }
