1ace945b7c778fd3703c8b2e1c1f977a22abedfc
Juan Duzac <juan.duzac@naranjax.com>
2022-10-22 19:47:14 -0300

    Creation of the Navigation between composables
    Created Navigation composable for navigating screens with their routes
    Created Screen sealed class with each Screen as classes

--- app/src/main/java/com/juanduzac/vendorlust/presentation/navigation/Screen.kt
+++ app/src/main/java/com/juanduzac/vendorlust/presentation/navigation/Screen.kt
@@ -1,4 +1,24 @@
 package com.juanduzac.vendorlust.presentation.navigation
 
-class Screen {
+sealed class Screen(val route: String){
+    object VendorListScreen : Screen("vendor_list_screen")
+    object VendorDetailScreen : Screen("vendor_detail_screen")
+
+    fun withArgs(vararg args: String): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/$arg")
+            }
+        }
+    }
+
+    fun withArgs(vararg args: Long): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/$arg")
+            }
+        }
+    }
 }
