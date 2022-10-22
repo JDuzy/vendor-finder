8026e7e175722a4b833e8510eaa7cda34efd9dca
Juan Duzac <juan.duzac@naranjax.com>
2022-10-22 19:46:09 -0300

    Added necessary mapping functions

--- app/src/main/java/com/juanduzac/vendorlust/data/mappers/GalleryMapper.kt
+++ app/src/main/java/com/juanduzac/vendorlust/data/mappers/GalleryMapper.kt
@@ -1,6 +1,7 @@
 package com.juanduzac.vendorlust.data.mappers
 
 import com.juanduzac.vendorlust.data.local.entities.GalleryItemEntity
+import com.juanduzac.vendorlust.data.local.relations.GalleryItemAndImage
 import com.juanduzac.vendorlust.data.remote.dtos.GalleryItemDto
 import com.juanduzac.vendorlust.domain.model.GalleryItem
 
@@ -15,5 +16,12 @@ fun GalleryItemDto.toGalleryItemEntity(vendorId: Long): GalleryItemEntity {
     return GalleryItemEntity(
         galleryItemId = id,
         vendorId = vendorId
+    )
+}
+
+fun GalleryItemAndImage.toGalleryItem(): GalleryItem {
+    return GalleryItem(
+        id = galleryItemEntity.galleryItemId,
+        image = imageEntity.toImage()
     )
 }
