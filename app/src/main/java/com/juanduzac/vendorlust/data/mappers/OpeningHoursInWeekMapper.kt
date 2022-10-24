361549174db2d81b18a7f9e12d85c8f92be70aeb
Juan Duzac <juan.duzac@naranjax.com>
2022-10-24 15:01:39 -0300

    Refactor in OpeningHoursInWeek:
    instead of a property for each day of the week, now the list of OpeningHoursInDay is a list
    
    Created Day ENUM for each day of the week
    Implemented getWeeklyOpeningHoursText()

--- app/src/main/java/com/juanduzac/vendorlust/data/mappers/OpeningHoursInWeekMapper.kt
+++ app/src/main/java/com/juanduzac/vendorlust/data/mappers/OpeningHoursInWeekMapper.kt
@@ -1,17 +1,27 @@
 package com.juanduzac.vendorlust.data.mappers
 
+import com.juanduzac.vendorlust.data.local.entities.OpeningHoursInWeekEntity
 import com.juanduzac.vendorlust.data.remote.dtos.OpeningHoursInWeekDto
 import com.juanduzac.vendorlust.domain.model.OpeningHoursInWeek
 
 fun OpeningHoursInWeekDto.toOpeningHoursInWeek(): OpeningHoursInWeek {
     return OpeningHoursInWeek(
         id = id,
-        monday = monday?.map { it.toOpeningHoursInDay() },
-        tuesday = tuesday?.map { it.toOpeningHoursInDay() },
-        wednesday = wednesday?.map { it.toOpeningHoursInDay() },
-        thursday = thursday?.map { it.toOpeningHoursInDay() },
-        friday = friday?.map { it.toOpeningHoursInDay() },
-        saturday = saturday?.map { it.toOpeningHoursInDay() },
-        sunday = sunday?.map { it.toOpeningHoursInDay() },
+        weeklyOpeningHours = listOf(
+            monday?.map { it.toOpeningHoursInDay() },
+            tuesday?.map { it.toOpeningHoursInDay() },
+            wednesday?.map { it.toOpeningHoursInDay() },
+            thursday?.map { it.toOpeningHoursInDay() },
+            friday?.map { it.toOpeningHoursInDay() },
+            saturday?.map { it.toOpeningHoursInDay() },
+            sunday?.map { it.toOpeningHoursInDay() }
+        )
+    )
+}
+
+fun OpeningHoursInWeekDto.toEntity(vendorId: Long): OpeningHoursInWeekEntity {
+    return OpeningHoursInWeekEntity(
+        openingHoursInWeekId = id,
+        vendorId = vendorId
     )
 }
---
+++ app/src/main/java/com/juanduzac/vendorlust/domain/model/Day.kt
@@ -0,0 +1,11 @@
+package com.juanduzac.vendorlust.domain.model
+
+enum class Day(val string: String, val int: Int) {
+    MONDAY("Monday", 0),
+    TUESDAY("Tuesday", 1),
+    WEDNESDAY("Wednesday", 2),
+    THURSDAY("Thursday", 3),
+    FRIDAY("Friday", 4),
+    SATURDAY("Saturday", 5),
+    SUNDAY("Sunday", 6)
+}
