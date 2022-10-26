7fcf738e842691890755d7a29a8c836a2426871a
Juan Duzac <juan.duzac@naranjax.com>
2022-10-26 14:40:28 -0300

    More efficient fetching and caching.
    if fetchFromRemote should be forced, then no searching queries on cache will be done.
    
    no more clearing db before inserting, when conflict on insert there is a replace
    
    VendorList now shows when response is done, instead of waiting to insert all vendors into cache.
    Insertion to cache continues while main thread is usable
    
    Added CircularProgressIndicator while loading vendors

--- app/src/main/java/com/juanduzac/vendorlust/presentation/MainActivity.kt
+++ app/src/main/java/com/juanduzac/vendorlust/presentation/MainActivity.kt
@@ -12,12 +12,12 @@ import com.juanduzac.vendorlust.presentation.navigation.Navigation
 import com.juanduzac.vendorlust.presentation.ui.theme.VendorLustTheme
 import dagger.hilt.android.AndroidEntryPoint
 
-
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             VendorLustTheme {
                 Navigation(
